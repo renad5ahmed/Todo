ce221ab47b7f53a4cb0c59f4af81cf30fc95a8ed
renad5ahmed <renadahmed581@gmail.com>
2025-01-16 22:15:08 +0200

    setting up project

--- todoApp/README.md
+++ todoApp/README.md
@@ -1,4 +1,4 @@
-# TodoList
+# TodoApp
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.0.4.
 
--- todoApp/src/app/app.component.spec.ts
+++ todoApp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'TodoList' title`, () => {
+  it(`should have the 'todoApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TodoList');
+    expect(app.title).toEqual('todoApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TodoList');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, todoApp');
   });
 });
